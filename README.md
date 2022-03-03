0cb404c891758163f10baa8b3ce0a02f887523b4
NedislavKolev <83339930+NedislavKolev@users.noreply.github.com>
2022-03-03 10:18:41 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+# netget.py is Python script for gathering network information
+
+**Usage**
+
+Can be used for CTFs (CaptureTheFlag) or just for cheking your network
+
+You can find the output files in "netgetresults" directory
+
+By default ***nikto*** and ***gobuster*** are using ports 80,443 and 8080
+
+**Execution**
+
+`./netget.py -ip 10.10.10.5`
+
+By default it uses ip address 10.10.10.10
+
+If you need help
+
+`./netget.py -h`
+<hr>
+<hr>
---
+++ netget.py
@@ -0,0 +1,44 @@
+#! /usr/bin/python
+
+import argparse
+import os
+
+
+parser = argparse.ArgumentParser(description='This script is used to gather information about systems network, '
+                                             'ports and web services')
+# Argument to run the script with
+parser.add_argument("-ip", "--ip-address", help="Supplies an IP Address, defaults to network: 10.10.10.10",
+                    default="10.10.10.10")
+parser.add_argument("-o", "--output", help="Sets file name for the output stream. \n"
+                                           "Defaults to date of the execution", default="Scan")
+# Parsing the argument
+args = parser.parse_args()
+
+print('Doing scan for IP Address: ' + args.ip_address)
+print('Outputing the files to infogetresults/')
+
+ipa = args.ip_address
+outstream = args.output
+
+# Commands executed by the script
+cmd = 'mkdir netgetresults'
+cmd1 = 'nmap -T4 -n -Pn -p* -o netgetresults/' + outstream + '_nmap_allports.txt ' + ipa
+cmd2 = 'nmap -A -T4 -n -Pn -p* -o netgetresults/' + outstream + '_nmap_deepscan.txt ' + ipa
+cmd3 = 'nikto -o netgetresults/' + outstream + '_nikto.txt -p 80,443,8080 -host ' + ipa
+cmd4 = 'gobuster dir -v -e -u http://' + ipa + ' -w /usr/share/wordlists/dirb/common.txt -o netgetresults/' + outstream + '_gobuster.txt'
+
+# Executing the commands
+os.system(cmd)
+print("Results directory created")
+os.system(cmd1)
+print("Nmap scan for all ports...")
+print("DONE")
+os.system(cmd2)
+print("Nmap deep scan...")
+print("DONE")
+os.system(cmd3)
+print("Nikto scan...")
+print("DONE")
+os.system(cmd4)
+print("GoBuster scan...")
+print("DONE")
